cc4617f5bf9f6449b03fe036411fe47d8c631053
Galatia <ptrivedius@aol.com>
2021-09-15 14:24:28 -0400

    connected database to node index file

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
 
-/*
-view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-*/
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'employee_db',
+},
+console.log('Connected!')
+);
 
 inquirer
     .prompt([
