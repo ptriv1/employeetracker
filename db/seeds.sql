22a622256aff0139212e5d4d70da79c507ab6d56
Galatia <ptrivedius@aol.com>
2021-09-18 17:35:01 -0400

    pushing current code

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+USE employee_db;
+
+INSERT INTO department (id, name)
+VALUE (1, "manager");
+
+INSERT INTO role (id, title, salary, department_id)
+VALUE  (1000, "manager", 50000, 1);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUE (10000, "John", "Richards", 1000, null);  
+       
