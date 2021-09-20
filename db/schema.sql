b6b470827c57ae5a4d5b840618b4a93e1bc2d2be
Galatia <ptrivedius@aol.com>
2021-09-20 10:30:52 -0400

    added AUTO_INCREMENT to role table in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -28,7 +28,7 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT
