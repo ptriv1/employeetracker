73ee625270cf2970484477e06f596dfa65ffa395
Galatia <ptrivedius@aol.com>
2021-09-20 07:50:04 -0400

    pushing current code

--- index.js
+++ index.js
@@ -204,5 +204,5 @@ WHEN I choose to update an employee role
 THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
 */
 function updateEmployeeRole() {
-    console.log("role");
+    
 }
